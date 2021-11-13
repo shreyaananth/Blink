09e4b98adb1c590454facd2cf3427d5197b70df2
Shreya Ananth <shreya_ananth@yahoo.com>
2021-11-13 11:01:18 +0530

    sqlitetest

--- blink/src/main/java/Client.java
+++ blink/src/main/java/Client.java
@@ -1,6 +1,20 @@
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Client {
     public static void main(String[] args){
-        System.out.println("test");
-        System.out.println("test23");
+        try {
+            try {
+                Class.forName("org.sqlite.JDBC");
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            Connection c = DriverManager.getConnection("jdbc:sqlite:client.db");
+            System.out.println("test");
+            System.out.println("test23");
+        } catch (SQLException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Opened database successfully");
     }
 }
