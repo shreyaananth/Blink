ca4fc81856b459000753a3c4be27f864947ae398
Shreya Ananth <shreya_ananth@yahoo.com>
2021-11-13 11:11:11 +0530

    sqlite0

--- blink/src/main/java/Client.java
+++ blink/src/main/java/Client.java
@@ -10,8 +10,8 @@ public class Client {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             }
             Connection c = DriverManager.getConnection("jdbc:sqlite:client.db");
-            System.out.println("test");
-            System.out.println("test23");
+            Statement stmt = c.createStatement();
+            c.close();
         } catch (SQLException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
