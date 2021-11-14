4cf06e7eb6d2dc35985fe8a6ba5a62c4b1328b65
Shreya Ananth <shreya_ananth@yahoo.com>
2021-11-14 12:53:49 +0530

    networks

--- server/src/main/java/server.java
+++ server/src/main/java/server.java
@@ -1,3 +1,19 @@
-public class server {
-    
+import java.io.*;  
+import java.net.*;  
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class server {  
+    public static void establish(){
+        try {
+            ServerSocket ss=new ServerSocket(6666);
+            Socket s=ss.accept();//establishes connection
+            ObjectInputStream dis=new ObjectInputStream(s.getInputStream());
+            
+        
+            ss.close();
+        } catch (IOException ex) {
+            Logger.getLogger(server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }      
 }
